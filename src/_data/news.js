0d38769c9a945f8b0561c71068781898607ce116
deanvlue <munoz.josecarlos@gmail.com>
2020-03-18 19:35:54 -0600

    Changed API name and trigger deployment

--- src/_data/news.js
+++ src/_data/news.js
@@ -2,14 +2,14 @@ const axios = require('axios');
 require('dotenv').config();
 
 
-const API_KEY=process.env.API_KEY;
+const NEWS_API_KEY=process.env.NEWS_API_KEY;
 
-const URL=`http://newsapi.org/v2/top-headlines?country=mx&apiKey=${API_KEY}&pageSize=5`;
+const URL=`http://newsapi.org/v2/top-headlines?country=mx&apiKey=${NEWS_API_KEY}&pageSize=5`;
 
  module.exports = async function (){
     try{
         const response = await axios.get(URL);
-        console.log(response.data);
+        //console.log(response.data);
         return response.data;
 
     } catch (error){
